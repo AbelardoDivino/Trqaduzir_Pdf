db1ee1c723e0096cb00144e69dc4e4dc517d5aca
AbelardoDivino <aaaaaaaaaaasafafe@gmail.com>
2026-07-16 20:46:25 -0300

    updating the backend routes

--- backend/serve.js
+++ backend/serve.js
@@ -1,7 +1,9 @@
+// validar dados de login
 require('dotenv').config()
 const mysql = require('mysql2')
 const expres = require('express')
 const cors = require('cors')
+const bcrypt = require('bcrypt')
 const app = expres()
 app.use(cors())
 
@@ -26,23 +28,154 @@ app.listen(PORT,()=>{
 console.log("servidor rodando na porta",PORT)
 })
 
-app.get('/',(req,res)=>{
+app.get('/usuarios',(req,res)=>{
+conectar.query(
+ 'select * from usuarios',
+  (err,res)=>{
+    if (err) {
+        return res.status(500).json(err)
+    }
+    res.json(res)
+  }
+);
+})
+
+app.get('/usuarios/id',(req,res)=>{
+conectar.query(
+'select * from usuarios where id = ?',
+[id],
+(err,res)=>{
+    if (err) {
+        return res.status(500).json(err)
+    }
+    res.json(res)
+}
+);
+})
+
+app.get('/admin',(req,res)=>{
+    conectar.query(
+        'select * from admin',
+        (err,res)=>{
+            if (err) {
+                return res.status(500).json(err)
+            }
+            res.json(res)
+        }
+    );
+})
+
+app.get('/admin/id',(req,res)=>{
+    conectar.query(
+        'select * from admin where id = ?',
+        [id],
+        (err,res)=>{
+            if (err) {
+                return res.status(500).json(err)
+            }
+            res.json(res)
+        }
+    );
+})
+
+app.post('/usuarios/login',(req,res)=>{
+
+const {nome,senha,email} = req.body;
+    conectar.query(
+'insert into usuarios (nome,senha,email) values (?,?,?)',
+[nome,senha,email],
+(err,res)=>{
+    if (err) {
+        return res.status(500).json({
+            erro:err.message
+        })
+    }
+    res.status(201).json({
+        mensagem:"cadastro realizado com sucesso",
+        id:res.insertId
+    })
+}
+
+
+);
+})
+
+
+app.post('/admin/login',(req,res)=>{
+    const {nome,senha} = req.body;
+
+    conectar.query(
+        'insert into admin(nome,senha) values(?,?)',
+        [nome,senha],
+        (err,res)=>{
+            if (err) {
+                return res.status(500).json({
+                    erro:err.message
+                })
+            }
+            res.status(201).json({
+                mensagem:"Administrator cadastrado com sucesso",
+                id:res.insertId
+            })
+        }
+    );
+})
+
+app.put('/usuarios/:id',(req,res)=>{
+    const {id} = req.params;
+    const {nome,senha,email} = req.body;
+    conectar.query(
+        'update usuarios set nome = ? , senha =  ?, email = ?, where id = ?',
+        (err,res)=>{
+            if (err) {
+                return res.status(500).json({erro:err.message})
+            }
+            res.json({mensagem:"usuario atualizado"})
+        }
+    );
+})
+
+app.put('/admin/:id',(req,res)=>{
+const {id} = req.params;
+const {nome,senha} = req.body;
+conectar.query(
+    'update admin set nome = ?, senha =? , where id = ?',
+    (err,res)=>{
+        if (err) {
+            return res.status(500).json({erro:err.message})
+        }
+        res.json({mensagem:"administrador cadastrado"})
+    }
+);
+
 
 })
 
-app.get('/',(req,res)=>{
-
+app.delete('/usuarios/deletar/:id',(req,res)=>{
+    const  {id} = req.params;
+    conectar.query(
+        'delete * from usuarios where id = ?',
+        (err,res)=>{
+            if (err) {
+                return res.status(500).json({erro:err.mensage})
+            }
+            res.json({mensagem:"usuario deletado"})
+        }
+    );
 })
 
-app.post('',(req,res)=>{
 
-})
+app.delete('/admin/deletar/:id',(req,res)=>{
 
-app.put('',(req,res)=>{
+    const {id} = req.params;
+    conectar.query(
+        'delete * from admin where id = ?',
+        (err,res)=>{
+            if (err) {
+                return res.status(500).json({erro:err.message})
+            }
+            res.json({mensagem:"admin deletado com sucesso"})
+        }
+    );
 
-})
-
-
-app.delete('',()=>{
-    
 })
---
+++ frontend/traduzir_pdf/src/components/Login.js
@@ -0,0 +1,15 @@
+// especificar o login usu e admin
+import { useState } from "react"
+import { useEffect } from "react"
+
+function Login(){
+
+    return <div>
+
+
+
+    </div>
+
+}
+
+export default Login
